f635533f9dadf0b931efe723b91f6eb920d19cad
mSumbur <m.sumbur@outlook.com>
2018-07-02 20:59:32 +0800

    优化

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
 // 元素
 var container = document.getElementById('game');
+// 判断是否有 requestAnimationFrame 方法，如果有则模拟实现
+window.requestAnimFrame =
+window.requestAnimationFrame ||
+window.webkitRequestAnimationFrame ||
+window.mozRequestAnimationFrame ||
+window.oRequestAnimationFrame ||
+window.msRequestAnimationFrame ||
+function(callback) {
+    window.setTimeout(callback, 1000 / 30);
+};
+
 // 动画对象
 var enemyANimation;
 // 等级
